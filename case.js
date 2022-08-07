ecaaf7fd6198a83dee31a509352b61f8a745b7a7
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-07 12:28:48 +0600

    case upper to lower

---
+++ case.js
@@ -0,0 +1,8 @@
+let userName = 'Shubho AhmEd';
+let userInput = 'ShuBho ahmed';
+if (userName.toLowerCase() === userInput.toLowerCase()) {
+    console.log('User is Valid');
+}
+else {
+    console.log('User is Invalid')
+}
