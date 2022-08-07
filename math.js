3b6c87b388a0da2ec1e117703483188d195014bb
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-07 14:28:45 +0600

    math explore

---
+++ math.js
@@ -0,0 +1,17 @@
+const result = Math.pow(2, 2);
+
+console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+const gap = Math.abs(num1 - num2);
+if (gap < 5) {
+    console.log('Make Friend')
+}
+else {
+    console.log('keep distance')
+}
+
+const number = 12.548;
+const result1 = Math.ceil(number);// we can use round ceil floor
+console.log(result1);
