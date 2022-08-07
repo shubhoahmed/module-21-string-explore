7a44170cca6dc8e0070a4321688bcf4faaf141e9
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-07 14:47:58 +0600

    swaping

--- temp.js
+++ temp.js
@@ -1,7 +1,16 @@
-let temp = 10;
+
 let x = 10;
 let y = 20;
+// console.log(x, y);
+
+const temp = x;
 x = y;
 y = temp;
+// console.log(x, y);
+// Anoter way to swaping
+let a = 5;
+let b = 7;
+[a, b] = [b, a];
+console.log(a, b)
+console.log(b, a)
 
-console.log(x, y);
