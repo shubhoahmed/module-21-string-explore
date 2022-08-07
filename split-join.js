c39a5536fa93f33e82fa0e7c18b801d1cf3470c8
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-07 13:38:33 +0600

    slice and split

---
+++ split-join.js
@@ -0,0 +1,6 @@
+const songLyrics = 'Amr sonar bangla. ami tumay valobashi. chiro din tumar aksh, tumar basts. amr pran a bajai bashi sunar bangla.'
+
+const parts = songLyrics.split(' ');
+const sentance = songLyrics.split('');
+const sentance1 = songLyrics.slice(0, 3);
+console.log(sentance1);
